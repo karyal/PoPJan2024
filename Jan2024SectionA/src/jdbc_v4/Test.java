571218c089dd5df6478c26279e896c1493b47be5
Krishna Aryal <info.karyal@gmail.com>
2024-03-18 08:23:49 +0545

    March 18 2024

--- Jan2024SectionA/src/jdbc_v4/Test.java
+++ Jan2024SectionA/src/jdbc_v4/Test.java
@@ -3,7 +3,7 @@ package jdbc_v4;
 public class Test {
 
 	public static void main(String[] args) {
-		//Create
+		//Creates
 		//Person person1 = new Person(21, "Name", "Address", "Email");
 		//new JDBC_V4().insertRecord(person1);
 		
@@ -15,10 +15,15 @@ public class Test {
 		//new JDBC_V4().searchRecord(1000);
 		
 		//Search Record based on PID
-		new JDBC_V4().searchRecord(1);//if found
-		Person tmpPerson=new Person(1 ,"Kiran Sharma", "Balaju", "kiran@gmail.com");
-		new JDBC_V4().updateRecord(tmpPerson);
-		new JDBC_V4().searchRecord(1);
+		//new JDBC_V4().searchRecord(1);//if found
+		//Person tmpPerson=new Person(1 ,"Kiran Sharma", "Balaju", "kiran@gmail.com");
+		//new JDBC_V4().updateRecord(tmpPerson);
+		//new JDBC_V4().searchRecord(1);
+		
+		//Delete Record
+		//new JDBC_V4().searchRecord(1);//search
+		//new JDBC_V4().deleteRecord(1); //delete record
+		//new JDBC_V4().searchRecord(1); //re-search
 	}
 
 }
