67a82dbf4afafc59d7e75d073adb094f0e45a42a
Krishna Aryal <info.karyal@gmail.com>
2024-03-19 09:57:38 +0545

    March 19 2024

--- Jan2024SectionA/src/jdbc_v5/PersonManager.java
+++ Jan2024SectionA/src/jdbc_v5/PersonManager.java
@@ -1,7 +1,12 @@
 package jdbc_v5;
 
+import java.util.ArrayList;
+
 public class PersonManager {
 	public boolean insertRecord(Person person) {
 		return new JDBC_V5().insertRecord(person);
+	}
+	public ArrayList<Person> selectAll() {
+		return new JDBC_V5().selectAll();
 	}
 }
